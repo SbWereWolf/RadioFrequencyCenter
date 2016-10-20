1dce9484849c4ad7e5083279116c3d9a8567f57d
SbWereWolf <ulfnew@gmail.com>
2016-10-20 23:37:26 +0500

    ~ fix public bool Proxy.InsertStations(List<RadioDevice> radioDeviceCollection) ~ fix public bool Proxy.DeleteAll() + development Sql scripts

---
+++ RadioFrequencyCenter.DataBrowser/Proxy/LinkStationFrequenciesToResdbFrq.cs
@@ -0,0 +1,15 @@
+﻿using RadioFrequencyCenter.DataBrowser.DataAccessLayer;
+
+namespace RadioFrequencyCenter.DataBrowser.Proxy
+{
+    public class LinkStationFrequenciesToResdbFrq
+    {
+        public LinkStationFrequenciesToResdbFrqRepository Repository { get; }
+
+        public LinkStationFrequenciesToResdbFrq()
+        {
+            var linkStationFrequenciesToResdbFrqRepository = new LinkStationFrequenciesToResdbFrqRepository();
+            Repository = linkStationFrequenciesToResdbFrqRepository;
+        }
+    }
+}
